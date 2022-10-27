43871618878a5218660dbffa112ac92e2cee897e
Lode Lesage <musilitar@gmail.com>
2022-10-27 15:58:02 +0200

    Add TypeScript support Add message component Add basic styling

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,22 @@
-<script>
+<script lang="ts">
+    import Message from './lib/Message.svelte'
+
+    const persistMessage = (event) => {
+        console.log(`Updated message: ${event.detail.message}`)
+    }
 </script>
 
 <main>
-    <p>Test</p>
+    <Message on:update={persistMessage} />
 </main>
 
 <style>
+    main {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        height: 100%;
+        background: linear-gradient(0deg, var(--pink) 0%, var(--yellow) 100%);
+    }
 </style>
